745cddbc427cc833660766ddfa9120c19ba38f3d
Darin Sorrentino <darin@redhat.com>
2016-05-23 19:37:27 -0400

    Added sudo to the crontab command

--- functions/undercloud/remote_undercloud_deploy.sh
+++ functions/undercloud/remote_undercloud_deploy.sh
@@ -10,6 +10,20 @@ stdout ""
 stdout "${SCRIPT_NAME} start"
 stdout ""
 
+SUDO=''
+if [[ "$(whoami)" != "root" ]]
+then
+  stdout "WARNING: Expected this to be run as root."
+  if [[ "$(sudo whoami)" != "root" ]]
+  then
+    stderr 'Terminating deployment.'
+    exit 1
+  else
+    stdout "Verified user has sudo capabilities.  Will use sudo as needed."
+    SUDO="sudo"
+  fi
+fi
+
 if [[ ! -f ~/undercloud.conf ]]
 then
   stderr "No undercloud.conf found. Exiting."
@@ -23,7 +37,7 @@ openstack undercloud install | tee -a ${LOG}
 if [[ ! -z "$(grep keystone /etc/passwd)" ]]
 then
   stdout "Adding keystone cronjob per: https://access.redhat.com/solutions/968883"
-  crontab -l -u keystone 2>/dev/null; echo '01 * * * * /usr/bin/keystone-manage token_flush >/dev/null 2>&1' | crontab -u keystone -
+  ${SUDO} crontab -l -u keystone 2>/dev/null; echo '01 * * * * /usr/bin/keystone-manage token_flush >/dev/null 2>&1' | ${SUDO} crontab -u keystone -
   if [[ $? -ne 0 ]]
   then
     stderr "Something went wrong with adding the cronjob."
